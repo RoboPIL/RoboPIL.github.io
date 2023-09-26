b786669ef10b4d110dd54ede8b64f9682d58fd8a
Yixuan Wang <yixuan22@illinois.edu>
2023-09-26 15:02:54 -0500

    add method

--- d3fields/static/js/index.js
+++ d3fields/static/js/index.js
@@ -1,6 +1,6 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
+var INTERP_BASE = "https://homes.cs.washington.edu/~kpar/nerfies/interpolation/stacked";
 var NUM_INTERP_FRAMES = 240;
 
 var interp_images = [];
@@ -58,13 +58,6 @@ $(document).ready(function() {
     	});
     }
 
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
     preloadInterpolationImages();
 
     $('#interpolation-slider').on('input', function(event) {
@@ -75,4 +68,7 @@ $(document).ready(function() {
 
     bulmaSlider.attach();
 
+    document.getElementById("single-task-result-video").playbackRate = 2.0;
+    document.getElementById("multi-task-result-video").playbackRate = 2.0;
 })
+
